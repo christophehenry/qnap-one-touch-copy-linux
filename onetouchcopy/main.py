0eee4213402765db3f5d91cad786df4f1842f809
Christophe Henry <contact@c-henry.fr>
2025-03-04 21:23:25 +0100

    Add systemd service

--- onetouchcopy/main.py
+++ onetouchcopy/main.py
@@ -1,16 +1,19 @@
 import asyncio
 import logging
+import os
 import sys
 from argparse import ArgumentParser
 from pathlib import Path
 from textwrap import dedent
 
+from dotenv import load_dotenv
 from evdev import list_devices as list_evdev, InputDevice
 
 from onetouchcopy.service import Udisks2Manager, Service
 from onetouchcopy.utils import await_sig, LogLevels
 
 KERNEL_MODULE_NAME = "qnap8528"
+DEST_ENV_VAR = "DEST"
 
 
 async def start():
@@ -23,10 +26,18 @@ async def start():
     )
 
     parser.add_argument(
-        "destination",
-        help="Directory were to copy the content of the USB disk",
+        "-d",
+        "--destination",
+        help=(
+            "Directory were to copy the content of the USB disk; "
+            f"will take priority over {DEST_ENV_VAR} environment variable"
+        ),
         action="store",
         type=str,
+    )
+
+    parser.add_argument(
+        "-c", "--config", help="Configuration file", action="store", default="/etc/onetouchcopy"
     )
 
     parser.add_argument("--verbose", "-v", action="count", default=0)
@@ -47,6 +58,11 @@ async def start():
     logging.basicConfig(**logger_config)
     logger = logging.getLogger("One touch copy daemon")
 
+    if not os.path.exists(args.config):
+        logger.warning(f"Config {args.config} file not found")
+    else:
+        load_dotenv(args.config)
+
     try:
         device = next(
             device
@@ -57,7 +73,16 @@ async def start():
         logger.critical(f"Firmware {KERNEL_MODULE_NAME} couldn't be found on the device")
         return 1
 
-    path = Path(args.destination)
+    destination = args.destination or os.getenv(DEST_ENV_VAR)
+
+    if not destination:
+        logger.critical(
+            f"Destination path not set. Please use -d option or the {DEST_ENV_VAR} environment "
+            "variable. Use --help for more information."
+        )
+        return 1
+
+    path = Path(destination)
     try:
         path.mkdir(parents=True, exist_ok=True)
     except OSError:
